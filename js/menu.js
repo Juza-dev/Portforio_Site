dad2a33d1acd376405ab01c6bf57735e9fc56916
yamato <yamato@zenoffice.co.jp>
2020-06-28 18:37:57 +0900

    add portforio_site

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const nav_bar_btn = document.querySelector('.toggle-btn')
+const nav_bar__toggle = document.querySelector('#nav-bar__toggle')
+const menu_bar = document.querySelector('#menu-bar')
+
+const showMenuEvent = (el) => {
+    el.addEventListener('click', (e) => {
+        e.stopPropagation();
+        nav_bar__toggle.classList.toggle('active')
+        menu_bar.classList.toggle('active')
+        window.scrollTo(0, 0)
+    })
+}
+
+showMenuEvent(nav_bar_btn)
+
+showMenuEvent(nav_bar__toggle)
